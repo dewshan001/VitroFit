661feca032d81e3205136eb56e5d16ad1241842b
dewshan001 <dewshangunawardhana@gmail.com>
2026-08-17 19:29:05 +0530

    initialize build outputs and configuration artifacts for BackendAPI and mobile project

--- BackendAPI/VitroFit.API/obj/Debug/net10.0/VitroFit.API.AssemblyInfo.cs
+++ BackendAPI/VitroFit.API/obj/Debug/net10.0/VitroFit.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitroFit.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c93c09b00817814ea398c3efc097d7f0b832ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b699fa82f041ec8df52b84db9a4b6aa75475cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitroFit.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitroFit.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
